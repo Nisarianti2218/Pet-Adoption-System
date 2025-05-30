cdfafd2244d3ca53a094768e01ab2596d0bfc560
nia <133296693+akhsaniaa02@users.noreply.github.com>
2025-05-30 21:07:43 +0700

    Update User.java

--- User.java
+++ User.java
@@ -1,23 +1,12 @@
 public class User {
-    private String id;
     private String name;
     private String phone;
 
-    public User(String id, String name, String phone) {
-        this.id = id;
+    public User(String name, String phone) {
         this.name = name;
         this.phone = phone;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
+    public String getName() { return name; }
+    public String getPhone() { return phone; }
 }
